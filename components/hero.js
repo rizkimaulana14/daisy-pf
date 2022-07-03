a97fc8a1938fda7e96a5bd8de7e693987ea29f20
rizkimaulana14 <rizkymaulana2085@gmail.com>
2022-07-03 19:07:00 +0700

    push

--- components/hero.js
+++ components/hero.js
@@ -2,31 +2,31 @@ import {AiFillGithub, AiFillLinkedin, AiFillInstagram} from "react-icons/ai"
 
 export default function hero() {
 	return (
-		<div class="hero min-h-screen" data-theme="dark">
-		  <div class="hero-content text-center">
-		    <div class="max-w-md">
-			    <div class="avatar mb-4">
-					  <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+		<div className="hero min-h-screen" data-theme="dark">
+		  <div className="hero-content text-center">
+		    <div className="max-w-md">
+			    <div className="avatar mb-4">
+					  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
 					    <img src="https://placeimg.com/192/192/people" />
 					  </div>
 					</div>
-		      <h3 class="text-3xl font-bold">Rizki Maulana</h3>
-		      <p class="py-2">
+		      <h3 className="text-3xl font-bold">Rizki Maulana</h3>
+		      <p className="py-2">
 		      	Hi everyone, my name is Rizki Maulana. <br /> 
 		      	I'm a Fullstack Web Developer.
 		      </p>
-		      <div class="flex flex-column gap-2 justify-center items-center mb-5">
-		      	<a class="btn btn-sm btn-secondary">
-		      		<AiFillGithub class="text-xl"></AiFillGithub>
+		      <div className="flex flex-column gap-2 justify-center items-center mb-5">
+		      	<a className="btn btn-sm btn-secondary">
+		      		<AiFillGithub className="text-xl"></AiFillGithub>
 		      	</a>
-		      	<a class="btn btn-sm btn-secondary">
-		      		<AiFillLinkedin class="text-xl"></AiFillLinkedin>
+		      	<a className="btn btn-sm btn-secondary">
+		      		<AiFillLinkedin className="text-xl"></AiFillLinkedin>
 		      	</a>
-		      	<a class="btn btn-sm btn-secondary">
-		      		<AiFillInstagram class="text-xl"></AiFillInstagram>
-		      	</a>
+		      	<a className="btn btn-sm btn-secondary">
+		      		<AiFillInstagram className="text-xl"></AiFillInstagram>
+		      	</a>O
 		      </div>
-		      <button class="btn btn-primary">Download CV</button>
+		      <button className="btn btn-primary">Download CV</button>
 		    </div>
 		  </div>
 		</div>
